044a36b44c4b085093b618b4814d07582fdd014f
reZerOR <gkraihan14k@gmail.com>
2024-08-25 18:51:53 +0600

    login api added

---
+++ src/pages/Login.tsx
@@ -0,0 +1,28 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
+const Login = () => {
+  const { register, handleSubmit } = useForm();
+
+  const [login, { data, error }] = useLoginMutation();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">Id</label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
+  );
+};
+
+export default Login;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,31 +1,34 @@
-import { createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import {  adminPaths} from "./admin.routes";
+import { adminPaths } from "./admin.routes";
 import { routeGenerator } from "../utiles/routesGenerator";
 import { facultyPaths } from "./faculty.routes";
-console.log(routeGenerator(facultyPaths));
-
+import Login from "../pages/Login";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>
-    },
-    {
-        path: "/admin",
-        element: <App/>,
-        children: routeGenerator(adminPaths)
-    },
-    {
-        path: "/faculty",
-        element: <App/>,
-        children: routeGenerator(facultyPaths)
-    },
-    {
-        path: "/student",
-        element: <App/>,
-        // children: adminRoutes
-    },
-])
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/admin",
+    element: <App />,
+    children: routeGenerator(adminPaths),
+  },
+  {
+    path: "/faculty",
+    element: <App />,
+    children: routeGenerator(facultyPaths),
+  },
+  {
+    path: "/student",
+    element: <App />,
+    // children: adminRoutes
+  },
+]);
 
-export default router
+export default router;
