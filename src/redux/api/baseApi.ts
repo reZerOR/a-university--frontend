290eea9a036110b012f82697488b9ee01e514969
reZerOR <gkraihan14k@gmail.com>
2024-09-02 15:16:32 +0600

    type errors in base api solved

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,4 +1,9 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryApi,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 import { logout, setUser } from "../features/auth/authSlice";
 
@@ -10,31 +15,36 @@ const baseQuery = fetchBaseQuery({
     if (token) {
       headers.set("authorization", `${token}`);
     }
-    return headers
+    return headers;
   },
 });
 
-const baseQueryWithRefreshToken = async (args, api, extraOptions)=>{
-  let result = await baseQuery(args, api, extraOptions)
-  if(result?.error?.status === 401){
-   const res = await fetch ('http://localhost:5000/api/v1/auth/refresh-token', {
-    method: 'POST',
-    credentials: 'include'
-   }) 
-   const data = await res.json()
-   if(data?.data?.accessToken){
-
-     const user = (api.getState() as RootState).auth.user
-     api.dispatch(setUser({
-      user,
-      token: data.data.accessToken
-     }))
-     result = await baseQuery(args, api, extraOptions)
-   }
-   api.dispatch(logout())
+const baseQueryWithRefreshToken = async (
+  args: FetchArgs,
+  api: BaseQueryApi,
+  extraOptions: object
+) => {
+  let result = await baseQuery(args, api, extraOptions);
+  if (result?.error?.status === 401) {
+    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
+    const data = await res.json();
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).auth.user;
+      api.dispatch(
+        setUser({
+          user,
+          token: data.data.accessToken,
+        })
+      );
+      result = await baseQuery(args, api, extraOptions);
+    }
+    api.dispatch(logout());
   }
-  return result
-}
+  return result;
+};
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
